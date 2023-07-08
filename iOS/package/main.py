9153d6d784349f959ecf5c58bf1724cc1d2da209
dioq <ptttqq@gmail.com>
2023-07-09 03:27:27 +0800

    获取iPhone udid

--- iOS/package/main.py
+++ iOS/package/main.py
@@ -52,16 +52,18 @@ def get_udid():
     """
     获取设备返回的值
     """
-    global udid_l
-    b_data = request.data
-    data_str = str(b_data).split('<?xml')[-1].split('</plist>')[0].split('dict')[1].replace('\\n', '').replace('\\t', '')\
-        .replace('>', '').replace('<', '').replace('/', '').replace('string', '').split('key')
-    udid = data_str[4]
-    product = app.get_phone(data_str[2])
-    version = data_str[6]
-    udid_l = [udid, product, version]
-    # 这里一定要对301进行重定向
-    return redirect(url_for('ios_udid.show_udid'), code=301)
+    print("get_udid -----------> go here")
+    return "hello, go here"
+    # global udid_l
+    # b_data = request.data
+    # data_str = str(b_data).split('<?xml')[-1].split('</plist>')[0].split('dict')[1].replace('\\n', '').replace('\\t', '')\
+    #     .replace('>', '').replace('<', '').replace('/', '').replace('string', '').split('key')
+    # udid = data_str[4]
+    # product = app.get_phone(data_str[2])
+    # version = data_str[6]
+    # udid_l = [udid, product, version]
+    # # 这里一定要对301进行重定向
+    # return redirect(url_for('ios_udid.show_udid'), code=301)
 
 
 # 实现通过浏览器下载并安装 安装包
