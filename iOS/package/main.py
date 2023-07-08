4dcd60c471d6fc61525a9cee71ea49c72718874e
dioq <ptttqq@gmail.com>
2023-07-09 03:14:54 +0800

    获取iPhone udid

--- iOS/package/main.py
+++ iOS/package/main.py
@@ -3,7 +3,7 @@
 from pathlib import Path
 
 import requests
-from flask import Flask, render_template, send_file, Response, request
+from flask import Flask, render_template, send_file, Response, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -42,11 +42,26 @@ def down_config():
     return response
 
 
-@app.route("/get_udid", methods=["GET", "POST"])
+# @app.route("/get_udid", methods=["GET", "POST"])
+# def get_udid():
+#     print(request.data)
+#     new_test_str = str(request.data, encoding='utf-8')
+#     print(new_test_str)
+@app.route('/get_udid/', methods=['GET', 'POST'])
 def get_udid():
-    print(request.data)
-    new_test_str = str(request.data, encoding='utf-8')
-    print(new_test_str)
+    """
+    获取设备返回的值
+    """
+    global udid_l
+    b_data = request.data
+    data_str = str(b_data).split('<?xml')[-1].split('</plist>')[0].split('dict')[1].replace('\\n', '').replace('\\t', '')\
+        .replace('>', '').replace('<', '').replace('/', '').replace('string', '').split('key')
+    udid = data_str[4]
+    product = app.get_phone(data_str[2])
+    version = data_str[6]
+    udid_l = [udid, product, version]
+    # 这里一定要对301进行重定向
+    return redirect(url_for('ios_udid.show_udid'), code=301)
 
 
 # 实现通过浏览器下载并安装 安装包
