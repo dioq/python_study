812cd7f2d9d7c4f3ce19d2faef170ab9eb499d17
dioq <ptttqq@gmail.com>
2023-07-09 02:31:55 +0800

    获取iPhone udid

--- iOS/package/main.py
+++ iOS/package/main.py
@@ -3,9 +3,7 @@
 from pathlib import Path
 
 import requests
-from flask import Flask, render_template, send_file, Response
-
-# from iOS.udid.main import xml_path
+from flask import Flask, render_template, send_file, Response, request
 
 app = Flask(__name__)
 
@@ -28,21 +26,26 @@ def down_config():
     """
     ios设备访问下载配置文件
     """
+
     def file_content(f_p):
         with open(f_p, 'rb') as f:
             return f.readlines()
-    filename="udid.mobileconfig"
+
+    filename = "udid.mobileconfig"
     file_path = "./files/udid.mobileconfig"
     response = Response(file_content(file_path))
+    print(response)
     # 这里的Content-Type一定要设置为application/x-apple-aspen-config
     response.headers['Content-Type'] = 'application/x-apple-aspen-config; chatset=utf-8'
     response.headers['Content-Disposition'] = 'attachment;filename="{}"'.format(filename)
+    print(response)
     return response
 
 
 @app.route("/get_udid", methods=["GET", "POST"])
 def get_udid():
-    new_test_str = str(requests.data, encoding='utf-8')
+    print(request.data)
+    new_test_str = str(request.data, encoding='utf-8')
     print(new_test_str)
 
 
