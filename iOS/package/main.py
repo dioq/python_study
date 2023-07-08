861fd994995fd432ac2077c62f52b423fa795bf6
dioq <ptttqq@gmail.com>
2023-07-09 00:47:06 +0800

    获取iPhone udid

--- iOS/package/main.py
+++ iOS/package/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 from pathlib import Path
+from urllib import request
 
 from flask import Flask, render_template, send_file
 
@@ -18,6 +19,15 @@ def download(filename):
     file_path = "./files/" + filename
     file = Path(file_path)
     return send_file(file, as_attachment=True)
+
+
+@app.route('/parse_udid', methods=['GET', 'POST'])
+def parse_udid():
+    """
+    解析获取udid数据
+    """
+    new_test_str = str(request.data, encoding='utf-8')
+    print(new_test_str)
 
 
 # 实现通过浏览器下载并安装 安装包
--- iOS/package/readme.md
+++ iOS/package/readme.md
@@ -1,7 +1,7 @@
 # iOS ipa包通过Safari在线安装
 Safari 浏览器有下载并安装ipa的权限。
 1. 先制作一个 html 网页, 从网页里下载一个 plist文件
-<a class="blink ios" href="itms-services://?action=download-manifest&url=https://jobs8.cn:9000/download/download.plist">
+<a class="blink ios" href="itms-services://?action=download-manifest&url=https://jobs8.cn:9000/download/install.plist">
     iOS App 在线安装
 </a>
 2. plist 文件里下载ipa包
